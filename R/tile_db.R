fa2972bd8cd4049a66ef517bd46b8833fa4892b6
Miles McBain <miles.mcbain@gmail.com>
2022-03-15 21:47:27 +1000

    get tile metadata from mbtiles

---
+++ R/tile_db.R
@@ -0,0 +1,24 @@
+open_tile_db <- function(path) {
+  db_con <- DBI::dbConnect(RSQLite::SQLite(), dbname = path)
+  db_con
+}
+
+get_tile_info(db_con) <- function() {
+  db_con |>
+    DBI::dbListTables()
+
+  metadata <-
+    DBI::dbReadTable(db_con, "metadata") |>
+    purrr::pmap(
+      \(name, value)
+       switch(name,
+        center = ,
+        bounds = setNames(list(as.numeric(strsplit(value, ",")[[1]])), name),
+        minzoom = ,
+        maxzoom = setNames(list(as.numeric(value)), name),
+        json = jsonlite::parse_json(value),
+        setNames(list(value), name)
+       )
+    ) |> 
+    purrr::flatten()
+}
